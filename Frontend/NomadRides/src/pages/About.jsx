47ef04beac0a4c7b05bc24ccef4d50086717b12d
JyotsanaAroraa <jyotsana.aroraa@gmail.com>
2025-11-02 10:00:00 +0530

    contact  page and about page

---
+++ Frontend/NomadRides/src/pages/About.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+
+import CommonSection from "../components/UI/CommonSection";
+import Helmet from "../components/Helmet/Helmet";
+import AboutSection from "../components/UI/AboutSection";
+import { Container, Row, Col } from "reactstrap";
+import BecomeVendorSection from "../components/UI/BecomeVendorSection";
+
+import driveImg from "../assets/all-images/drive.jpg";
+import OurMembers from "../components/UI/OurMembers";
+import "../styles/about.css";
+
+const About = () => {
+  return (
+    <Helmet title="About">
+      <CommonSection title="About Us" />
+      <AboutSection aboutClass="aboutPage" />
+
+      <section className="about__page-section">
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="12">
+              <div className="about__page-img">
+                <img src={driveImg} alt="" className="w-100 rounded-3" />
+              </div>
+            </Col>
+
+            <Col lg="6" md="6" sm="12">
+              <div className="about__page-content">
+                <h2 className="section__title">
+                  We Are Committed To Provide Safe Ride Solutions
+                </h2>
+
+                <p className="section__description">
+                We differentiate ourselves by offering a range of exceptional services designed to provide a seamless and trustworthy experience for our customers. Our offerings include access to the vehicle's maintenance history, along with recent photos and videos of the vehicle to ensure transparency. We also provide user reviews from the vehicle's last trip to help you make informed decisions.
+                </p>
+
+                <p className="section__description">
+                Every journey begins with a full fuel tank, and our services include insurance coverage for accidents and damages for your peace of mind. Unlike others, we require no security deposit or original document submission, making the process hassle-free. Additionally, we offer the best deals and flexible subscription plans to suit your needs.
+                </p>
+
+                <div className=" d-flex align-items-center gap-3 mt-4">
+                  <span className="fs-4">
+                    <i class="ri-phone-line"></i>
+                  </span>
+
+                  <div>
+                    <h6 className="section__subtitle">Need Any Help?</h6>
+                    <h4>+91-9927155685</h4>
+                  </div>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <BecomeVendorSection/>
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12" className="mb-5 text-center">
+              <h6 className="section__subtitle">Experts</h6>
+              <h2 className="section__title">Our Team Members</h2>
+            </Col>
+            <OurMembers />
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+export default About;
